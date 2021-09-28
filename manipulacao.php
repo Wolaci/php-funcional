cabf0da378169db6795c3ef40e8516795dd58458
Wolaci <wolacijose8@gmail.com>
2021-09-27 23:27:43 -0300

    Pegando código para ler

--- manipulacao.php
+++ manipulacao.php
@@ -5,40 +5,42 @@ $dados = require 'dados.php';
 $contador = count($dados);
 echo "Número de países: $contador\n";
 
-function somaMedalhas(int $medalhasAcumuladas, int $medalhas){
-  return $medalhasAcumuladas + $medalhas;
+function somaMedalhas(int $medalhasAcumuladas, int $medalhas) {
+    return $medalhasAcumuladas + $medalhas;
 }
 
-$brasil = $dados[0];
-$numeroMedalhas = array_reduce($brasil['medalhas'], 'somaMedalhas',0);
-
-function converteNomePaisParaMaiusculo(array $pais){
-  $pais['pais'] = mb_convert_case($pais['pais'], MB_CASE_UPPER); 
-  return $pais;
+function convertePaisParaLetraMaisucula(array $pais): array {
+    $pais['pais'] = mb_convert_case($pais['pais'], MB_CASE_UPPER);
+    return $pais;
 }
 
-function verificaPaisTemEspacoNoNome(array $pais){
-  return strpos($pais['pais'], ' ')!==false;
+function verificaSePaisTemEspacoNoNome(array $pais): bool
+{
+    return strpos($pais['pais'], ' ') !== false;
 }
 
-function medalhasTotaisDosPaises(int $medalhasAcumuladas, array $pais){
-  return $medalhasAcumuladas + array_reduce($pais['medalhas'], 'somaMedalhas', 0);
-}
+$dados = array_map('convertePaisParaLetraMaisucula', $dados);
+$dados = array_filter($dados, 'verificaSePaisTemEspacoNoNome');
 
-$dados = array_map('converteNomePaisParaMaiusculo', $dados);
+$medalhas = array_reduce(
+    array_map(function (array $medalhas) {
+        return array_reduce($medalhas, 'somaMedalhas', 0);
+    }, array_column($dados, 'medalhas')),
+    'somaMedalhas',
+    0
+);
+
+usort($dados, function (array $pais1, array $pais2) {
+    $medalhasPais1 = $pais1['medalhas'];
+    $medalhasPais2 = $pais2['medalhas'];
+
+    $comparacaoOuro = $medalhasPais2['ouro'] <=> $medalhasPais1['ouro'];
+    $comparacaoPrata = $medalhasPais2['prata'] <=> $medalhasPais1['prata'];
+    return $comparacaoOuro !== 0 ? $comparacaoOuro
+        : ($comparacaoPrata !== 0 ? $comparacaoPrata
+        : $medalhasPais2['bronze'] <=> $medalhasPais1['bronze']);
+});
+
 var_dump($dados);
-echo '<br>';
-echo "--------------------------------------";
-echo '<br>';
-$dados = array_filter($dados, 'verificaPaisTemEspacoNoNome');
-var_dump($dados);
-echo '<br>';
-echo "--------------------------------------";
-echo '<br>';
-$dados = array_filter(array_map('converteNomePaisParaMaiusculo', $dados), 'verificaPaisTemEspacoNoNome') ;
-var_dump($dados);
-echo '<br>';
-echo "--------------------------------------";
-echo '<br>';
 
-echo array_reduce($dados, 'medalhasTotaisDosPaises', 0);
+echo $medalhas;
