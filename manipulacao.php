cfa43da8629f4efff1dc928a33af88a5d12345da
Wolaci <wolacijose8@gmail.com>
2021-09-27 22:45:44 -0300

    Criando função e utilizando no array_map

--- manipulacao.php
+++ manipulacao.php
@@ -4,3 +4,10 @@ $dados = require 'dados.php';
 
 $contador = count($dados);
 echo "Número de países: $contador\n";
+
+function converteNomePaisParaMaiusculo(array $pais){
+  $pais['pais'] = mb_convert_case($pais['pais'], MB_CASE_UPPER); 
+  return $pais;
+}
+$dados = array_map('converteNomePaisParaMaiusculo', $dados);
+var_dump($dados);
