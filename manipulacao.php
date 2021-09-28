a861331fb88ecbb4f10a7343b70b884c20da1ef2
Wolaci <wolacijose8@gmail.com>
2021-09-27 22:59:57 -0300

    Filtrando com array_filter

--- manipulacao.php
+++ manipulacao.php
@@ -9,5 +9,20 @@ function converteNomePaisParaMaiusculo(array $pais){
   $pais['pais'] = mb_convert_case($pais['pais'], MB_CASE_UPPER); 
   return $pais;
 }
+
+function verificaPaisTemEspacoNoNome(array $pais){
+  return strpos($pais['pais'], ' ')!==false;
+}
+
 $dados = array_map('converteNomePaisParaMaiusculo', $dados);
+var_dump($dados);
+echo '<br>';
+echo "--------------------------------------";
+echo '<br>';
+$dados = array_filter($dados, 'verificaPaisTemEspacoNoNome');
+var_dump($dados);
+echo '<br>';
+echo "--------------------------------------";
+echo '<br>';
+$dados = array_filter(array_map('converteNomePaisParaMaiusculo', $dados), 'verificaPaisTemEspacoNoNome') ;
 var_dump($dados);
